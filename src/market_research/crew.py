2f45b157ce552ac9b8373a2e1348ef6d92a70351
Karthi Kuppan <karthi@jyoana.com>
2025-01-31 09:37:30 -0500

    Removed the sequential process from crew kickoff

--- src/market_research/crew.py
+++ src/market_research/crew.py
@@ -82,7 +82,7 @@ class MarketResearch:
         return Crew(
             agents=self.agents,  # Automatically created by the @agent decorator
             tasks=self.tasks,  # Automatically created by the @task decorator
-            process=Process.sequential,
+            # process=Process.sequential,
             verbose=True,
             # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
